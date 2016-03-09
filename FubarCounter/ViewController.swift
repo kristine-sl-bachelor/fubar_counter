2cd64d35545ee49b180bdd04977c3bd181aa9c5e
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2016-03-09 13:47:42 +0100

    Initial Commit

---
+++ FubarCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FubarCounter
+//
+//  Created by Kristine Sundt Lorentzen on 9/3/16.
+//  Copyright (c) 2016 Kristine Sundt Lorentzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
